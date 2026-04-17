9153faa55b927924fe2d2849d4138ce1a80f6b86
1179irfatkhan-web <1179.irfatkhan@gmail.com>
2026-04-17 12:03:30 +0530

    Update email configuration with placeholders

--- easygo/settings.py
+++ easygo/settings.py
@@ -145,5 +145,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '3124.khadijamansuri@gmail.com'
-EMAIL_HOST_PASSWORD = 'dbvh vswo kvvz xqhv' # Not your regular password
+EMAIL_HOST_USER = 'add your email'
+EMAIL_HOST_PASSWORD = 'add your app key ' # Not your regular password
